1ec461e0c863412d22e02339c9ab54a39f89a3bd
Fahima Abida Chowdhury <165580535+abida3564@users.noreply.github.com>
2025-02-28 19:24:09 +0600

    Add files via upload

---
+++ CodeforcesChecking.cpp
@@ -0,0 +1,36 @@
+//https://codeforces.com/problemset/problem/1791/A
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    char c;
+    for(int i=0;i<t;i++){
+      cin>>c;
+      switch(c){
+      case 'c':
+        cout<<"YES"<<endl;
+        break;
+         case 'o':
+        cout<<"YES"<<endl;
+        break;
+         case 'd':
+        cout<<"YES"<<endl;
+        break;
+         case 'e':
+        cout<<"YES"<<endl;
+        break;
+         case 'f':
+        cout<<"YES"<<endl;
+        break;
+         case 'r':
+        cout<<"YES"<<endl;
+        break;
+         case 's':
+        cout<<"YES"<<endl;
+        break;
+        default:
+            printf("NO");
+      }
+      cout<<endl;
+    }
+}

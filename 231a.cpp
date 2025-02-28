9f099f551e9837165aeea6aa4e1247ba5160cace
Fahima Abida Chowdhury <165580535+abida3564@users.noreply.github.com>
2025-02-28 19:26:34 +0600

    Add files via upload
    https://codeforces.com/problemset/problem/231/A

---
+++ 231a.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+   int n;cin>>n;
+   int count =0;
+   for(int i=0;i<n;i++){
+       int p,v,t;
+    cin>>p>>v>>t;
+    if((p==1 && v==1) || (p==1 && t==1 )||( v==1 && t==1)) count++;
+   }
+   cout<<count<<endl;
+     return 0;
+}

603dedd9a2c8ba6b62a371db9ccce76c21d7bf24
Fahima Abida Chowdhury <165580535+abida3564@users.noreply.github.com>
2025-03-01 23:57:56 +0600

    Add files via upload

---
+++ 546A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k,w;
+    long long int n;
+    cin>>k>>n>>w;
+    long long int ans=0;
+    for(int i=1;i<=w;i++){
+     ans+=(i*k);
+    }
+    long long b =ans-n;
+    if(b>0)
+    cout<<b<<endl;
+    else{
+        cout<<0<<endl;
+    }
+    return 0;
+}

cc76e490119587737df53eb2c348722da67ff997
Fahima Abida Chowdhury <165580535+abida3564@users.noreply.github.com>
2025-03-03 00:54:10 +0600

    Add files via upload

---
+++ 617A.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/617/A
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+
+    int step = (x+4)/5;
+    cout<<step<<endl;
+    return 0;
+}
